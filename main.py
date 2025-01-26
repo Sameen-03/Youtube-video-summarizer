8e2d642df8c6235e37847630498a1fdf751f45a6
Ameen <90543306+Sameen-03@users.noreply.github.com>
2025-01-26 15:39:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,10 @@
-"""
-import assemblyai as aai
-from pytubefix import YouTube
-import ollama
-
-#get the video
-def download_video():
-    yt_link = input("Enter the YouTube video URL: ")
-    try:
-        yt_object = YouTube(yt_link)
-        print(f"Downloading: {yt_object.title}")
-        video = yt_object.streams.get_highest_resolution()
-        video.download(filename="C:/Users/ameen/OneDrive/Desktop")
-    except Exception as e:
-        print(f"An error occurred when downloading the video: {e}")
-download_video()
-
-
-# get the transcript
-aai.settings.api_key="ed7a2d6f98c941a18a7b640c6bc35a1b"
-transcript = aai.Transcriber().transcribe("C:/Users/ameen/OneDrive/Desktop/test.mp4")
-subtitles = transcript.export_subtitles_srt()
-
-#feed the transcript to LLM
-client = ollama.Client()
-model = 'llama3.2:1b'
-prompt = f"summarize this video transcript for me {subtitles}"
-
-response = client.generate(model = model, prompt = prompt)
-print("Video summary:\n")
-print(response.response)
-"""
 import os
 import assemblyai as aai
 from pytubefix import YouTube
 import ollama
 
 # Set AssemblyAI API Key
-aai.settings.api_key = "ed7a2d6f98c941a18a7b640c6bc35a1b"  # Set your API key as an environment variable
+aai.settings.api_key = "Your_API_key"  # Set your API key as an environment variable
 yt_link = input("Enter the YouTube video URL: ")
 yt_object = YouTube(yt_link,'WEB')
 # Function to download the YouTube video
@@ -72,7 +40,7 @@ def summarize_transcript(transcript):
     try:
         print("Summarizing the transcript using Ollama...")
         client = ollama.Client()
-        model = 'llama3.2:1b'
+        model = 'name_of_your_local_model' #example: 'llama3.2:2b'
         prompt = f"Summarize this video transcript for me:\n\n{transcript}"
 
         response = client.generate(model=model, prompt=prompt)
